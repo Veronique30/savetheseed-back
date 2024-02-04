9b26d07a9505d05ac9861135f9b7afbb22862aef
Veronique Azzopardi <azzopardivero@gmail.com>
2024-02-04 07:47:34 +0100

    done

--- modules/checkBody.js
+++ modules/checkBody.js
@@ -1,14 +1,17 @@
 function checkBody(body, keys) {
-    let isValid = true;
-  
-    for (const field of keys) {
-      if (!body[field] || body[field] === '') {
-        isValid = false;
-      }
+  let isValid = true;
+
+  for (const field of keys) {
+    if (!body[field] || body[field] === "") {
+      isValid = false;
     }
-  
-    return isValid;
   }
-  
-  module.exports = { checkBody };
-  
+
+  return isValid;
+}
+
+module.exports = { checkBody };
+// Essentiellement, cette fonction est utilisée pour valider si certains champs
+// dans un objet (body) ont des valeurs non vides
+// Si l'un des champs spécifiés est manquant ou a une valeur vide,
+//  la fonction renvoie false; sinon, elle renvoie true.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
+//express facilite la creation d un backend qui jouera le rôle de webservice
+//c'est un framework qui va apporter des fonctionnalités clé en main et imposer une structure au projet
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
